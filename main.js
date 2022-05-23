e90ae95b92bd66868a82104a63b8887551534655
AlessP93 <alespiscino@libero.it>
2022-05-23 16:47:16 +0200

    problema html riga 28

--- main.js
+++ main.js
@@ -12,10 +12,20 @@ Descrizione:
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 const app = new Vue({
-    el: "#app"
+    el: "#app",
     data: {
-        todos: {
-            
-        }
-    }
-})
+        todos: 
+        {
+            modello: "Daytona"
+            colore: "Gold"
+        },
+        {
+            modello: "Submariner"
+            colore: "Silver"
+        },
+        {
+            modello: "Datejust"
+            colore: "Coral"
+        },
+    },
+});
