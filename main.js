201b7517b21cde92ac7b89b4b40811e56df8e727
AlessP93 <alespiscino@libero.it>
2022-05-23 17:04:10 +0200

    riga 20 error

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-Descrizione:
-// Rifare l'esercizio della to do list.
 // Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
+
 // Stampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
+
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-// MILESTONE 3
+
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 const app = new Vue({
@@ -16,16 +14,20 @@ const app = new Vue({
     data: {
         todos: 
         {
-            modello: "Daytona"
-            colore: "Gold"
+            text: "Fare la spesa",
+            done: "true",
         },
         {
-            modello: "Submariner"
-            colore: "Silver"
+            text: "Pagare le bollette",
+            done: "true",
         },
         {
-            modello: "Datejust"
-            colore: "Coral"
+            text: "Andare in palestra",
+            done: "false",
         },
-    },
+        {
+            text: "Chiamare il dentista",
+            done: "false",
+        },
+    }
 });
